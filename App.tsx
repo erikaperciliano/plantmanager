b44246ececb8ebc0c5c23a2c52b306503d849d91
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-04-21 12:22:55 -0300

    chamando a pilha de navegação

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 
-import { Welcome } from './src/pages/Welcome';
+import Routes from './src/routes';
 import {useFonts, Jost_400Regular, Jost_600SemiBold} from '@expo-google-fonts/jost';
 
 export default function App(){
@@ -14,6 +14,6 @@ export default function App(){
     return <AppLoading/>
 
   return (
-    <Welcome/>
+    <Routes/>
   )
 }
